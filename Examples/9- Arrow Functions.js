f4c9d2c6ab130b152a397dfb89b6508e37d6fc67
Camilo.IT <npgcamilo@gmail.com>
2018-12-19 09:41:33 -0500

    9- Arrow Functions

---
+++ Examples/9- Arrow Functions.js
@@ -0,0 +1,22 @@
+
+/**
+ * la forma mas comun de escribir una arror function asignandola a una variable
+ * @param {*} persona 
+ */
+const esMayorDeEdad = (persona)=>{
+    // do something
+}
+
+
+/**
+ * Cuando se recibe un solo parametro se puede obviar los parentesis
+ * @param {*} persona 
+ */
+const esMayorDeEdad =  persona =>{
+    // do something
+}
+
+
+
+
+
